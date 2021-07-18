2873f08e67dae245a2e4a89a76bdba3580b68378
treyturley <81778542+treyturley@users.noreply.github.com>
2021-07-18 09:19:42 -0500

    Initial commit for SpringBootHelloWorld project.

---
+++ SpringBootHelloWorld/src/main/java/com/app/controller/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.app.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+	
+	@RequestMapping("/")
+	public String sayHello() {
+		return "Hello world spring boot";
+	}
+	
+
+}
